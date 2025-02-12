ee6e345c6889020a2a7b760a2b16aec8b286bed4
Abdul Bari <abdulbari3635@gmail.com>
2025-02-12 21:19:11 +0300

    wakeup page

--- sendTimeData.js
+++ sendTimeData.js
@@ -42,9 +42,6 @@ function calculateBedtime(hours, minutes){
     <button id="goBack">Go back</button>
   </div>
 `;
-
-
-
   })
 }
 
@@ -70,5 +67,23 @@ function calculateWakeupTime(hours, minutes){
     for (const [cycle, time] of Object.entries(data)) {
       console.log(`${cycle}: ${time}`);
     }
+    let dataContainer = document.querySelector(".container")
+    dataContainer.innerHTML = `<div class="wakeup-container">
+    <h3 class="page-subheading">Wake-up time</h3>
+    <p>The average human takes 15 minutes to fall asleep.</p>
+    <p>If you go to sleep right now, you should try to wake up at one of the following times:</p>
+    <div class="display-time-container">
+      ${Object.entries(data)
+        .map(([cycle, time], index) => `
+          <div class="time-box-${index < 2 ? 'suggested' : 'rest'}">
+            ${time}
+          </div>
+        `)
+        .join("")}
+    </div>
+    <p>If you wake up at one of these times, you’ll rise in between 90-minute sleep cycles. A good night’s sleep consists of 5-6 complete sleep cycles.</p>
+    <button id="goBack">Go back</button>
+  </div>
+`;
   })
 }
